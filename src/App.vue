2ac420d37d05cc0ee99fdb7e59bfdf23e72f5cfa
YouROCK <yourock88@meta.ua>
2025-01-31 18:59:35 +0200

    Refactor App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,39 @@
 <script>
-  export default {
-    data() {
-      return {
-        filmName: '',
-        filmNames: [],
-        username: '',
-        text: '',
-        comments: [],
-        editingFilm: null,
-        editingText: '',
-      }
+export default {
+  computed: {
+    filmComments() {
+      return this.comments.filter(comment => comment.film === this.selectedFilm)
     },
-    methods: {
-      addFilmName() {
-        this.filmNames.push(this.filmName)
-        this.filmName = ''
-      },
-      addComment() {
-        const existingComment = this.comments.find(
-          c => c.filmName === this.filmName
-        )
-        if (existingComment) {
-          existingComment.text = this.text
-          existingComment.username = this.username
-        } else {
-          this.comments.push({
-            filmName: this.filmName,
-            username: this.username,
-            text: this.text,
-          })
-        }
-        this.username = ''
-        this.text = ''
-      },
-      editComment(filmName) {
-        const comment = this.comments.find(c => c.filmName === filmName)
-        if (comment) {
-          this.editingFilm = filmName
-          this.editingUsername = comment.username
-          this.editingText = comment.text
-        } else {
-          this.editingFilm = filmName
-          this.editingUsername = ''
-          this.editingText = ''
-        }
-      },
-      saveEditedComment() {
-        if (this.editingFilm) {
-          const comment = this.comments.find(
-            c => c.filmName === this.editingFilm
-          )
-          if (comment) {
-            comment.text = this.editingText
-            comment.username = this.editingUsername
-          } else {
-            this.comments.push({
-              filmName: this.editingFilm,
-              username: this.username,
-              text: this.editingText,
-            })
-          }
-          this.editingFilm = null
-          this.username = ''
-          this.editingText = ''
-        }
-      },
+  },
+
+  data() {
+    return {
+      comments: [],
+      films: [],
+      newFilm: '',
+      newCommentUsername: '',
+      newCommentText: '',
+      selectedFilm: '',
+    }
+  },
+
+  methods: {
+    addFilm() {
+      this.films.push(this.newFilm)
+      this.newFilm = ''
     },
-  }
+
+    addComment() {
+      this.comments.push({
+        username: this.newCommentUsername,
+        text: this.newCommentText,
+        film: this.newFilm,
+      })
+      this.newCommentUsername = ''
+      this.newCommentText = ''
+    },
+  },
+}
 </script>
 
 <template>
@@ -82,6 +49,8 @@
           v-on:dblclick="editComment(film)"
         >
           {{ film }}
+
+          <input type="radio" name="selectedFilm" />
         </li>
       </ul>
       <h3>Добавить фильм</h3>
@@ -126,62 +95,62 @@
 </template>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  @import url(https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,regular,500,600,700,800,900,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic);
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+@import url(https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,regular,500,600,700,800,900,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic);
 
-  * {
-    box-sizing: border-box;
-  }
+* {
+  box-sizing: border-box;
+}
 
-  .flex {
-    display: flex;
-  }
+.flex {
+  display: flex;
+}
 
-  .flex > * {
-    flex: 1;
-  }
+.flex > * {
+  flex: 1;
+}
 
-  div {
-    padding: 5px;
-    /* border: 1px solid #000; */
-  }
+div {
+  padding: 5px;
+  /* border: 1px solid #000; */
+}
 
-  input,
-  textarea,
-  html {
-    font-family: 'Nunito Sans';
-    color: #234;
-    font-size: 18px;
-    font-weight: 600;
-  }
+input,
+textarea,
+html {
+  font-family: 'Nunito Sans';
+  color: #234;
+  font-size: 18px;
+  font-weight: 600;
+}
 
-  ul {
-    list-style: none;
-  }
+ul {
+  list-style: none;
+}
 
-  textarea {
-    resize: none;
-  }
+textarea {
+  resize: none;
+}
 
-  .comments li i {
-    display: inline-block;
-    text-indent: 12px;
-    font-weight: 400;
-  }
+.comments li i {
+  display: inline-block;
+  text-indent: 12px;
+  font-weight: 400;
+}
 
-  .wrap-input input,
-  .wrap-input textarea {
-    width: 100%;
-  }
+.wrap-input input,
+.wrap-input textarea {
+  width: 100%;
+}
 
-  .wrap-button {
-    /* text-align: center; */
-  }
+.wrap-button {
+  /* text-align: center; */
+}
 </style>
