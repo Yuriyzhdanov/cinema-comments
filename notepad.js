a44a6e1a95ca25f46a35768b464dfe6ac91ab725
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2025-02-04 18:01:34 +0200

    rename file main.js-notepad.js

---
+++ notepad.js
@@ -0,0 +1,12 @@
+const filmNames = ['matrix', 'tenet']
+const comments = [
+  { username: 'Витя', text: 'классно', filmName: 'matrix' },
+  { username: 'Женя', text: 'очень скучный', filmName: 'tenet' },
+]
+
+console.log(comments[0])
+
+const arr = filmNames[0]
+const nam = comments[0].filmName
+nam
+console.log(arr === nam)
